4916144da73f28974cf7dffda13aec00ab6dd54a
Konstantin Timofeev <mrfamone@mail.ru>
2024-10-29 21:07:38 +0300

    fix: gen container

--- src/pages/services/[serv]/[brand]/[model]/[gen]/index.vue
+++ src/pages/services/[serv]/[brand]/[model]/[gen]/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="bg-gray-2 pt-10 md:pt-20" style="min-height: calc(100vh - 506px);">
-    <div class="container mx-auto">
+    <div class="container mx-auto px-4 md:px-0">
       <UiFormsCarSearch />
     </div>
     <SearchModsNew :mods="data" />
