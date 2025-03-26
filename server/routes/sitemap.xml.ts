db47cd69bc89d9cbca9d60ad747006b203bf71e1
Konstantin Timofeev <39969686+famone@users.noreply.github.com>
2025-03-27 01:43:57 +0300

    fix: sitemap

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,22 +1,30 @@
 import { SitemapStream, streamToPromise } from 'sitemap'
 import axios from 'axios'
-// import { serverQueryContent } from '#content/server'
 
-export default defineEventHandler(async (event: any) => {
+export default defineEventHandler(async (event) => {
   const protocol = event.node.req.headers['x-forwarded-proto'] || 'https'
   const host = event.node.req.headers.host || 'rechip-tuning.ru'
   const hostname = `${protocol}://${host}`
 
-  // Fetch all documents
+  // Устанавливаем заголовок для корректного отображения XML
+  setResponseHeader(event, 'Content-Type', 'application/xml')
+
   const sitemap = new SitemapStream({ hostname })
 
-  // fetch pages
-  const { data } = await axios.get('https://api.rechip-tuning.ru/sitemap.txt')
-  const pages = data.split('\n')
-  for (const page of pages as any) {
-    sitemap.write({ url: `${sitemap.hostname}${page}`, changefreq: 'daily' })
-  }
+  try {
+    // Получаем список URL из sitemap.txt
+    const { data } = await axios.get('https://api.rechip-tuning.ru/sitemap.txt')
 
-  sitemap.end()
-  return streamToPromise(sitemap)
+    const pages = data.split('\n').filter((page) => page.trim() !== '')
+
+    for (const page of pages) {
+      sitemap.write({ url: page.trim(), changefreq: 'daily' })
+    }
+
+    sitemap.end()
+    return streamToPromise(sitemap)
+  } catch (error) {
+    console.error('Ошибка при генерации Sitemap:', error)
+    throw createError({ statusCode: 500, message: 'Ошибка генерации Sitemap' })
+  }
 })
